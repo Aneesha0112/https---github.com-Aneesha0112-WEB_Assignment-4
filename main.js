5fba4ace07c555b52f75496fbf4ebed297695ef0
Aneesha0112 <115572496+Aneesha0112@users.noreply.github.com>
2022-11-18 12:56:46 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// getUsers button 
+document.getElementById("btn-users").addEventListener('click', getUsers);
+
+function getUsers() {
+  fetch("http://localhost:3000/users/")
+  .then((res)=> res.json())
+  .then((data) => console.log(data))
+  .catch((err)=> console.log(err))
+}
 const login1=document.getElementById("loginpage");
 const reg=document.getElementById("regpage");
 const note=document.getElementById("notepage");
